31cebf52064642da18cc97cb00f5ecc0c1e68818
Simon Oulevay <simon.oulevay@gmail.com>
2014-10-09 21:26:15 +0200

    Configured slim to play nice with angular.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  # Pretty-print slim templates
-  Slim::Engine.set_default_options pretty: true
 end
---
+++ config/initializers/slim.rb
@@ -0,0 +1,9 @@
+
+slim_options = {
+  # Disable {} attributes to play nice with angular
+  attr_delims: { '(' => ')', '[' => ']' }
+}
+
+slim_options[:pretty] = true if Rails.env == 'development'
+
+Slim::Engine.set_default_options slim_options
