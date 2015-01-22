eeb55dfd2fcf27e1c58c9ea0479dcf513f0b2cce
Simon Oulevay <simon.oulevay@gmail.com>
2015-01-22 15:37:46 +0100

    Missing file.

--- spec/angular/lair.js
+++ spec/angular/lair.js
@@ -1,16 +1,21 @@
 
 
-angular.module('lair', ['lair.state', 'lair.auth', 'lair.home', 'lair.routes', 'lair.items.edit'])
+angular.module('lair', ['lair.state', 'lair.auth', 'lair.home', 'lair.routes', 'lair.items.edit', 'satellizer'])
 
   // configuration
   .constant('version', "0.1.0")
   .constant('environment', "test")
   .constant('config.googleOAuth2ClientId', "test")
-  .constant('config.googleOAuth2CallbackUrl', "/users/auth/google_oauth2/callback")
 
   // enable debug log unless in production
   .config(['environment', '$logProvider', function(env, $logProvider) {
     $logProvider.debugEnabled(env !== 'production');
+  }])
+
+  .config(['config.googleOAuth2ClientId', '$authProvider', function(googleOAuth2ClientId, $authProvider) {
+    $authProvider.google({
+      clientId: googleOAuth2ClientId
+    });
   }])
 
 ;
