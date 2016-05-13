bbf9c987d2ecc071f16634daac92379095bed950
Simon Oulevay <simon.oulevay@gmail.com>
2016-05-13 23:51:10 +0200

    Resque web.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 require 'resque/server'
 
 Rails.application.routes.draw do
+
+  resque_web_constraint = lambda do |request|
+    #current_user = request.env['warden'].user
+    #current_user.present? && current_user.respond_to?(:is_admin?) && current_user.is_admin?
+    true
+  end
+
+  constraints resque_web_constraint do
+    mount ResqueWeb::Engine => '/resque'
+  end
+
   namespace :auth, module: nil do
     post '/google', to: 'security#google'
     post '/token', to: 'security#token' # TODO: move this to API
@@ -9,7 +20,6 @@ Rails.application.routes.draw do
   mount Lair::API => '/api'
 
   if %w(development test).include? Rails.env
-    mount Resque::Server.new, at: '/resque'
     get '/templates/*template', to: 'home#template'
   end
 
