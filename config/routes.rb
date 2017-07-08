83a2af5db00028aa87242c10a52519015f58d763
Simon Oulevay <simon.oulevay@gmail.com>
2017-07-08 19:01:50 +0200

    Mount resque in development

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,10 @@ require 'resque/server'
 
 Rails.application.routes.draw do
 
-  # FIXME: deploy resque web separately
-  #resque_web_constraint = lambda do |request|
-    #current_user = request.env['warden'].user
-    #current_user.present? && current_user.respond_to?(:is_admin?) && current_user.is_admin?
-  #  true
-  #end
-
-  #constraints resque_web_constraint do
-  #  mount ResqueWeb::Engine => '/resque'
-  #end
+  # FIXME: deploy resque web separately in production
+  if Rails.env == 'development'
+    mount ResqueWeb::Engine => '/resque'
+  end
 
   namespace :auth, module: nil do
     post '/google', to: 'security#google'
